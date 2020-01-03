90759051c348f412f2b094ec411ac738207d35d5
William Rivera <willitorivera@gmail.com>
2020-01-03 08:08:36 -0500

    Initial commit

---
+++ EtoTest/EtoTest.Desktop/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Eto.Forms;
+using Eto.Drawing;
+using EtoOtherForm;
+
+namespace EtoTest.Desktop
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            new Application(Eto.Platform.Detect).Run(new MainOtherForm());
+        }
+    }
+}
